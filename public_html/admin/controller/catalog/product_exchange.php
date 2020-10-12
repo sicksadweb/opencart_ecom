d9bc6583fc2b34e5d20adbb7bc535d15735915ef
sicksadweb <50939694+sicksadweb@users.noreply.github.com>
2020-10-12 09:05:48 +0700

    Revert "add accordeon to admin panel"

--- public_html/admin/controller/catalog/product_exchange.php
+++ public_html/admin/controller/catalog/product_exchange.php
@@ -3,58 +3,41 @@
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 class ControllerCatalogProductExchange extends Controller {
-	private $error = array();	
+	private $error = array();
 
 	public function index() {
-		
-		//print_r($this->request);
-
-		$url = '';
-		//$line_number_in_exel = 0;
-		
 		$this->load->language('catalog/product');
 
 		$this->document->setTitle($this->language->get('heading_title'));
 
 		$this->load->model('catalog/product');
-		
-		$data['token'] = $this->session->data['user_token'];
-
+	
 		require_once DIR_STORAGE.'exchange/SimpleXLSX.php';
 
-		if ( $xlsx = SimpleXLSX::parse(DIR_STORAGE.'exchange/123.xlsx') ) { 
-
+		if ( $xlsx = SimpleXLSX::parse(DIR_STORAGE.'exchange/price.xlsx') ) { 
 
 			foreach ( $xlsx->rows() as $k => $r ) {
-				
-				/* $line_number_in_exel++;	
-				if ($line_number_in_exel < 10) continue;
-				if ($line_number_in_exel == 2000) break; */
-				
-				$product = $this->model_catalog_product->getProductExchange($r);
-				//print_r($product);	
-				if ($product){
-					
-					//print_r($data['products']);
 
-					if ($r[2] == null && $r[3] == null ) continue;
+				if ($r[2] == 1 && $r[3] == 0 ){
 
-					//There is a quantity, no price
-					if ($r[2] == 1 && $r[3] == 0){					
-						$data['error'][] = array(
-							'name' => $r[1],
-							'price' => $this->currency->format($r[3], $this->config->get('config_currency')),
-							'status' => $r[2],	
-							'sku' => $r[0],	
-							'class' => 'danger',
-							'href_shop'  => HTTP_CATALOG . 'index.php?route=product/product&product_id=' . $product['product_id'],
-							'edit'       => $this->url->link('catalog/product/edit', 'user_token=' . $this->session->data['user_token'] . '&product_id=' . $product['product_id'] . $url, true)	
-						);
-					}
-					
-					//There is a quantity, There is a price
+					$this->model_catalog_product->getProductExchange($r);
+					$data['error'][] = array(
+						'name' => $r[1],
+						'price' => $this->currency->format($r[3], $this->config->get('config_currency')),
+						'status' => $r[2],	
+						'sku' => $r[0],	
+						'class' => 'danger',
+						'href_shop'  => HTTP_CATALOG . 'index.php?route=product/product&product_id=' . $product['product_id'],
+						'edit'       => $this->url->link('catalog/product/edit', 'user_token=' . $this->session->data['user_token'] . '&product_id=' . $product['product_id'] . $url, true)
+							
+					);
+
+				 } else {
+
+					$product = $this->model_catalog_product->getProductExchange($r);
+
+					//---------
 					if ($r[2] == 1 && $r[3] > 0 ) {
-
 						$data['products'][] = array(
 							'product_id' => $product['product_id'],
 							'name' => $r[1],
@@ -64,13 +47,12 @@ class ControllerCatalogProductExchange extends Controller {
 							'class' => 'success',	
 							'href_shop'  => HTTP_CATALOG . 'index.php?route=product/product&product_id=' . $product['product_id'],
 							'edit'       => $this->url->link('catalog/product/edit', 'user_token=' . $this->session->data['user_token'] . '&product_id=' . $product['product_id'] . $url, true)
-															
+			
+													
 						);			
 					}
 					
-					//no quantity, There is a price 
 					if ($r[2] == 0 && $r[3] > 0 ) {
-						
 						$data['products'][] = array(
 							'name' => $r[1],
 							'price' => $this->currency->format($r[3], $this->config->get('config_currency')),
@@ -83,9 +65,7 @@ class ControllerCatalogProductExchange extends Controller {
 						);	
 					}
 					
-					//no quantity, no price 
 					if ($r[2] == 0 && $r[3] == 0 ) {
-						
 						$data['products'][] = array(
 							'name' => $r[1],
 							'price' => $this->currency->format($r[3], $this->config->get('config_currency')),
@@ -96,38 +76,18 @@ class ControllerCatalogProductExchange extends Controller {
 							'edit'       => $this->url->link('catalog/product/edit', 'user_token=' . $this->session->data['user_token'] . '&product_id=' . $product['product_id'] . $url, true)
 									
 						);	
-					}
-				}else{
-					
-					if ($r[2] == null && $r[3] == null ) continue;
+					}						//---------
 
-					//There is quantity OR There is price, product for adding 
-					if ($r[2] == 1 || $r[3] > 0){					
-						$data['newProducts'][] = array(
-							'name' => $r[1],
-							'price' => $this->currency->format($r[3], $this->config->get('config_currency')),
-							'status' => $r[2],	
-							'sku' => $r[0],	
-							'class' => 'danger'								
-						);
-					//no quantity, no price 
-					}else{
-						$data['products'][] = array(
-							'name' => $r[1],
-							'price' => $this->currency->format($r[3], $this->config->get('config_currency')),
-							'status' => $r[2],
-							'sku' => $r[0],	
-							'class' => 'warning',	
-							'href_shop'  => HTTP_CATALOG . 'index.php?route=product/product&product_id=' . '',
-							'edit'       => $this->url->link('catalog/product/edit', 'user_token=' . $this->session->data['user_token'] . '&product_id=' . '' . $url, true)
-						);	
-					}
-				}			
-			}	
-						  	
-		} else {
-				echo SimpleXLSX::parseError();
+
+
+				 }
+
 			}
+//			print_r ($data['products'].'<br>');
+//			print_r ($data['error'].'<br>');		  	
+		} else {
+			echo SimpleXLSX::parseError();
+		}
 
 
 		$data['header'] = $this->load->controller('common/header');
